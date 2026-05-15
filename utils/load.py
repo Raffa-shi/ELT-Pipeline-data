f8704d9671caec6937c4fecae7a5b3c0acde265e
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-05-15 18:31:14 +0700

    adjust

--- utils/load.py
+++ utils/load.py
@@ -15,7 +15,7 @@ def save_to_csv(df: pd.DataFrame, filename="products.csv"):
         print(f"[ERROR] Gagal save CSV: {e}")
 
 
-def save_to_google_sheets(df: pd.DataFrame):
+def save_to_google_sheets(df):
 
     try:
 
@@ -31,21 +31,27 @@ def save_to_google_sheets(df: pd.DataFrame):
 
         client = gspread.authorize(creds)
 
-        spreadsheet = client.open("Fashion Studio ETL")
+        spreadsheet = client.open(
+            "Fashion Studio ETL"
+        )
 
         worksheet = spreadsheet.sheet1
 
         worksheet.clear()
 
+        # FIX TIMESTAMP
+        df["timestamp"] = df["timestamp"].astype(str)
+
         worksheet.update(
-            [df.columns.values.tolist()] + df.values.tolist()
+            [df.columns.values.tolist()] +
+            df.values.tolist()
         )
 
         print("[INFO] Google Sheets berhasil disimpan")
 
     except Exception as e:
-        print(f"[ERROR] Gagal upload Google Sheets: {e}")
 
+        print(f"[ERROR] Google Sheets: {e}")
 
 def save_to_postgresql(df: pd.DataFrame):
 
